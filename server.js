42e1eb8188c0646fa5b5b344a1cc6e3151409ccf
Kawã Kleber <kawaklebersc@gmail.com>
2025-03-14 15:41:55 -0300

    Commit

---
+++ server.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
+const prisma = new PrismaClient()
+
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.post('/users',async (req, res) => {
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    })
+    res.status(201).json(req.body)
+})
+
+app.get('/users', async (req, res) => {
+    let users = []
+
+    if(req.query){
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                email: req.query.email,
+                age:req.query.age,
+            },
+        })
+    }else{
+        users = await prisma.user.findMany()
+    }
+    res.status(200).json(users)
+})
+app.put('/users/:id',async (req, res) => {
+    await prisma.user.update({
+        where: {
+            id: req.params.id,
+        },
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    })
+    res.status(201).json(req.body)
+})
+
+app.delete('/users/:id',async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id,
+        },
+    })
+    res.status(200).json({message: "Sucess Deteler User!"})
+})
+app.listen
+(3100)
+
+//Password auth
+'45Sq5ojJzFFJgZtc'
